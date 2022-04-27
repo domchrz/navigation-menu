0d92c1f9b3e4d6fddea7d17af91ebacbb4d425c5
Dominik Chrzescijanek <D.Chrzescijanek@itechart-group.com>
2022-04-27 17:44:21 +0200

    cleaned Dropdown styles

--- src/components/Dropdown/styles.jsx
+++ src/components/Dropdown/styles.jsx
@@ -4,12 +4,8 @@ export const DropdownContainer = styled.div`
   position: absolute;
   display: flex;
   flex-direction: column;
-  ${props => props.renderOrigin.top && `top: ${props.renderOrigin.top}px`};
-  ${props => props.renderOrigin.left && `left: ${props.renderOrigin.left}px`};
-  ${props =>
-    props.renderOrigin.right && `right: ${props.renderOrigin.right}px`};
-  ${props =>
-    props.renderOrigin.bottom && `bottom: ${props.renderOrigin.bottom}px`};
+  ${props => `top: ${props.renderOrigin.top}px`};
+  ${props => `left: ${props.renderOrigin.left}px`};
   z-index: 1;
 
   &.mount {
